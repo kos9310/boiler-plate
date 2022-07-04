bb82316eb405a92829150d050a5201ab074e82c9
kos9310 <kos9310@gmail.com>
2022-07-04 14:03:40 +0900

    nodeJS Auth,Logout 기능만들기 / React 설치

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser')
-const config = require('./config/dev.js');
-
+const config = require('./server/config/dev.js.js');
+const { auth } = require('./middleware/auth')
 const { User } = require("./models/User");
 
 //appication/x-www-form-urlencoded
@@ -22,7 +22,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World! 언제 집에 갈까요? 집에 가고 싶습니다.'))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
   // 회원 가입할 때 필요한 정보들을 client에서 가져오면
   // 그것들을 데이터 베이스에 넣어준다.
@@ -35,7 +35,7 @@ app.post('/register', (req, res) => {
   })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
   // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err, user) => {
@@ -63,4 +63,30 @@ app.post('/login', (req, res) => {
     });
 })
 
+app.get('/api/users/auth', auth, (req, res) => {
+
+  // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.usr.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+
+  User.findOneAndUpdate({ _id: req.user._id}, 
+    { token: "" }
+  , (err, user) => {
+    if(err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true
+    })
+  })
+})
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
